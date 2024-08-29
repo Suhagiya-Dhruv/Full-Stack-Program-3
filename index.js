f70a87857c312a42e687dc0b5e7b843101ea55f9
Hardi Desai <hardidesai799@gmail.com>
2024-08-29 11:44:51 +0530

    HTTP Server

--- index.js
+++ index.js
@@ -1,35 +1,28 @@
-// const { sum, minus } = require("./opration")
-// // const { minus } = require("./opration")
+const http = require('http');
+const fs = require('fs');
 
-// const ans = minus(10, 20)
+function serverFunction(req, res) {
+    if (req.url === '/products') {
+        const data = fs.readFileSync('product.json', 'utf-8');
+        return res.end(data)
+    } else if (req.url === '/user') {
+        const data = fs.readFileSync('user.json', 'utf-8');
+        return res.end(data)
+    }
+    return res.end("Invalid URL: " + req.url);
+}
 
-// console.log(ans);
+const server = http.createServer(serverFunction);
 
+// const server = http.createServer((req, res) => {
+//     if (req.url === '/products') {
+//         const data = fs.readFileSync('product.json', 'utf-8');
+//         return res.end(data)
+//     } else if (req.url === '/user') {
+//         const data = fs.readFileSync('user.json', 'utf-8');
+//         return res.end(data)
+//     }
+//     return res.end("Invalid URL: " + req.url);
+// });
 
-// File system
-
-/*
-Read,
-create,
-detele
-update
-*/
-
-const fs = require('fs')
-
-
-// const data = fs.readFileSync('hello.txt', 'utf-8') // path, encode -  read
-
-// const data = fs.writeFileSync('hello.txt', '<h1>File 1</h1>') // new file, overwrite - create
-
-// const data = fs.unlinkSync('hello.txt') // delete
-
-// const data = fs.appendFileSync('test.txt', '\nline 3');
-
-fs.readFile('hello.txt', 'utf-8', (a, b) => {
-    console.log(a, b)
-})
-
-// console.log(data)
-
-// task : httpserver
+server.listen(5200);
