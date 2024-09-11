b26c60c9ec88e799d950908e8f75ec8e13c60f66
Dhruv Suhagiya <dhruv.suhagiya11@gmail.com>
2024-09-11 11:17:02 +0530

    Error Handling

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const UserScheme = require('./user');
+const bcrypt = require('bcrypt');
 
 const server = express();
 
@@ -14,10 +15,54 @@ mongoose.connect(url)
 const port = 6000;
 
 server.post('/create', async function (req, res) {
-    const data = req.body;
-    await UserScheme.create(data)
-    console.log(data);
-    res.send('Data Received!');
+    // Error Handling
+    try {
+        const data = req.body;
+
+        const userFind = await UserScheme.findOne({ email: req.body.email })
+
+        if (userFind) {
+            return res.send("Email already exits")
+        }
+
+        const password = await bcrypt.hash(req.body.password, 10);
+
+        await UserScheme.create({ ...data, email: req.body.email.toLowerCase(), password })
+        return res.send('Data Received!');
+
+    } catch (err) {
+        return res.send(err)
+    }
+})
+
+server.post('/login', async function (req, res) {
+    // Error Handling
+    try {
+        const data = req.body;
+
+        const userFind = await UserScheme.findOne({ email: req.body.email.toLowerCase() })
+
+        if (!userFind) {
+            return res.send("Account not exits")
+        }
+
+        const password = await bcrypt.compare(req.body.password, userFind.password);
+
+        if (!password) {
+            return res.send("Invalid password");
+        }
+
+        return res.send('Login Successfully');
+
+    } catch (err) {
+        return res.send(err)
+    }
+})
+
+server.get("/user", async function (req, res) {
+    const data = await UserScheme.find({}, { password: 0, __v:0 }); // {find}, {}
+
+    return res.send(data)
 })
 
 server.listen(port, () => {
